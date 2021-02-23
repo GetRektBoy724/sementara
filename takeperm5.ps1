3919401391d5a9bc399a02bbc468ae2f41de3f99
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-02-23 14:47:38 +0700

    Create takeperm5.ps1

---
+++ takeperm5.ps1
@@ -0,0 +1,12 @@
+$AddACL = New-Object System.Security.AccessControl.RegistryAccessRule ("Everyone","FullControl","ObjectInherit,ContainerInherit","None","Allow")
+$owner = [System.Security.Principal.NTAccount]"Administrators"
+$keyCR = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey("CLSID\{76A64158-CB41-11D1-8B02-00600806D9B6}",[Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,[System.Security.AccessControl.RegistryRights]::takeownership)
+# Get a blank ACL since you don't have access and need ownership
+$aclCR = $keyCR.GetAccessControl([System.Security.AccessControl.AccessControlSections]::None)
+$aclCR.SetOwner($owner)
+$keyCR.SetAccessControl($aclCR)
+# Get the acl and modify it
+$aclCR = $keyCR.GetAccessControl()
+$aclCR.SetAccessRule($AddACL)
+$keyCR.SetAccessControl($aclCR)
+$keyCR.Close()
